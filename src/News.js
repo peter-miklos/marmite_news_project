ed36a87a72869246ebaa0a7e368bd02d991bd44c
jameshamann <jameshamann@me.com>
2016-09-30 10:47:52 +0100

    battery dying switch drivers

--- src/News.js
+++ src/News.js
@@ -1,16 +1,21 @@
 function News () {
   this.articles = []
+  this.anything;
   // this._link = link;
 }
 
 News.prototype = {
   getArticles: function() {
+    var receivedData;
     var request = new XMLHttpRequest();
-    request.open('GET', 'https://spy-api.herokuapp.com/apis?api-key=65c03619b403f5635e19f324e6f9ac0890f5a857&json=guardian', true);
+    var promise = request.open('GET', 'https://spy-api.herokuapp.com/apis?api-key=65c03619b403f5635e19f324e6f9ac0890f5a857&json=guardian', true);
     request.onload = function() {
       if (request.status >= 200 && request.status < 400) {
     // Success!
         var data = JSON.parse(request.responseText);
+        this.anything = data;
+        // receivedData = data;
+
       } else {
     // We reached our target server, but it returned an error
       }
@@ -19,9 +24,16 @@ News.prototype = {
     // There was a connection error of some sort
     };
     request.send();
-    return request;
+    // return request;
+    setTimeout(function()
+    {console.log(this.anything);}, 5000);
   },
-  createArticles(api) {
+
+  // promise.done(function() {
+  //   return(receivedData);
+  // })
+    createArticles(api) {
     this.articles.push(api);
+
   }
 };
