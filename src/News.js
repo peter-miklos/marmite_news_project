40eb14703f8d03f7b11f86975fc4182ed1c6e381
BenRoss92 <benjaminjackross@gmail.com>
2016-09-30 12:10:54 +0100

    news model returns api data

--- src/News.js
+++ src/News.js
@@ -1,32 +1,38 @@
 function News () {
-  this.articles = []
-  this.anything;
+  this.articles = [];
   // this._link = link;
 }
 
-News.prototype = {
-  getArticles: function() {
-    var receivedData;
-    var request = new XMLHttpRequest();
-    var promise = request.open('GET', 'https://spy-api.herokuapp.com/apis?api-key=65c03619b403f5635e19f324e6f9ac0890f5a857&json=guardian', true);
-    request.onload = function() {
-      if (request.status >= 200 && request.status < 400) {
-    // Success!
-        var data = JSON.parse(request.responseText);
-        this.anything = data;
-        // receivedData = data;
+var promise = new Promise(function(resolve, reject) {
 
-      } else {
-    // We reached our target server, but it returned an error
-      }
-    };
-    request.onerror = function() {
+  var request = new XMLHttpRequest();
+  request.open('GET', 'https://spy-api.herokuapp.com/apis?api-key=65c03619b403f5635e19f324e6f9ac0890f5a857&json=guardian', true);
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+      resolve(JSON.parse(request.responseText));
+
+    } else {
+      reject('error happened :(:(:(')
+      // We reached our target server, but it returned an error
+    }
+  };
+  request.onerror = function() {
     // There was a connection error of some sort
-    };
-    request.send();
-    // return request;
-    setTimeout(function()
-    {console.log(this.anything);}, 5000);
+  };
+  request.send();
+})
+
+News.prototype = {
+
+
+  getArticles: function() {
+    promise.then(function(result) {
+      console.log(result);
+    },
+    function(error) {
+      console.log(error);
+    });
   },
 
   // promise.done(function() {
